988857b068cbc25d1a2abaa7e40ffc35a15a0a62
Anders Bille <anders.bille.lind@gmail.com>
2021-05-09 17:28:51 +0200

    blank

--- reactscratch/webpack.config.js
+++ reactscratch/webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                options: { presets: ["@babel/env"]}
+                use: { loader: 'babel-loader' }, 
+                //options: { presets: ["@babel/env"]} options is shortcut for rule.use => then i don't need?
             },
             {
                 test: /\.css$/,
@@ -21,14 +21,14 @@ module.exports = {
     },
     resolve: { extensions: ['*', '.js', '.jsx']},
     output: {
-        path: path.resolve(__dirname, 'dist/'),
-        publicPath: '/dist/',
+        path: path.resolve(__dirname, '/dist'),
+        //publicPath: '/dist/',
         filename: 'bundle.js'
     },
     devServer: {
         contentBase: path.join(__dirname, 'public/'),
         port: 3000,
-        publicPath: 'http://localhost:3000/dist/',
+        //publicPath: 'http://localhost:3000/dist/',
         hotOnly: true
     },
     plugins: [new webpack.HotModuleReplacementPlugin()]
